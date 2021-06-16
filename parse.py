08eebd3df0c1514df5d1315990e02c15bcf72388
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2021-06-16 14:16:36 +0200

    Retrieve blame information

--- parse.py
+++ parse.py
@@ -2,6 +2,7 @@ import sys
 from glob import glob
 import os.path
 import re
+import git
 
 req_re = re.compile(
     r"^\|\s*\*\*([0-9.]+)\*\* \|\s*(\[[^\]]*\])?\s*(.*)\s*\|\s*(.*)\s*\|\s*(.*)\s*\|\s*(.*)\s*\|"
@@ -23,17 +24,44 @@ def level(l1, l2, l3):
 
 
 def parse_file(fname):
+    reqs = []
     with open(fname) as fp:
         for lineno, line in enumerate1(fp):
             m = req_re.match(line)
             if m:
                 id, tag, req, l1, l2, l3 = m.groups()
                 l = level(l1, l2, l3)
-                print(id, l)
+                reqs.append((id, tag, req, l, (fname, lineno)))
+    return reqs
+
+
+class AsvsRepo:
+    def __init__(self, path):
+        self.path = path
+        self.repo = git.Repo(path)
+
+    @property
+    def requirement_file_paths(self):
+        return glob(os.path.join(self.path, "4.0/en/0x??-V*"))
+
+    def blame(self, req_file):
+        blame = self.repo.blame("HEAD", req_file)
+        line_no = 1
+        line_to_commit = {}
+        for commit, lines in blame:
+            new_line_no = line_no + len(lines)
+            while line_no < new_line_no:
+                line_to_commit[line_no] = commit
+                line_no += 1
+        return line_to_commit
 
 
 if __name__ == "__main__":
     asvs_dir = sys.argv[1]
-    req_files = glob(os.path.join(asvs_dir, "0x??-V*"))
+    repo = AsvsRepo(asvs_dir)
+
+    req_files = repo.requirement_file_paths
     for req_file in req_files:
+        print(req_file)
+        blame = repo.blame(req_file)
         print(parse_file(req_file))
